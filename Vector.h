e71bddb2cb4f51ec7fee58138e4e52b6800882c8
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2023-12-17 18:48:16 +0100

    finished vector definitions: constructor, destructor, operator [], at, front, back, getSize, getCapacity, empty, shrinkToFit, reserveSpace, push_back

--- Vector.h
+++ Vector.h
@@ -33,7 +33,7 @@ namespace Collections{
             bool empty()const; //returns true if empty
             //Modifiers:
             void shrinkToFit(); //shrinks capacity to size
-            void reserveSpace(size_t extra_capacity); //reserve extra memory
+            void reserveSpace(size_t extra_space); //reserve extra memory
             void push_back(const T& value); //push value at the end
             void push_front(const T& value); //push value in front
             void pop_back(); //pop last value
@@ -45,8 +45,111 @@ namespace Collections{
             T* begin(); //returns pointer to the first element
             T* end(); //returns pointer to the last element
         private:
+            void init_vector();
             size_t capacity;
             size_t size;
             T* vec;
     };
+}
+
+template <typename T>
+void Collections::Vector<T>::init_vector(){
+    if (vec == nullptr){
+        T* new_vec = (T*)malloc(5 * sizeof(T));
+        if (new_vec == nullptr){
+            throw std::bad_alloc();
+        }else{
+            vec = new_vec;
+            capacity = 5;
+            size = 0;
+        }
+    }else{
+        throw std::runtime_error("Can not initialize new vector");
+    }
+}
+
+template <typename T>
+T& Collections::Vector<T>::operator[](size_t index){
+    if (index >= size){
+        throw std::out_of_range("Index out of range");
+    }
+    return vec[index];
+}
+
+template <typename T>
+T& Collections::Vector<T>::at(size_t index){
+    if (index > size){
+        throw std::out_of_range("Index out of range");
+    }
+    return vec[index];
+}
+
+template <typename T>
+T& Collections::Vector<T>::front(){
+    if (size == 0){
+        throw std::out_of_range("Vector is empty");
+    }
+    return vec[0];
+}
+
+template <typename T>
+T& Collections::Vector<T>::back(){
+    if (size == 0){
+        throw std::out_of_range("Vector is empty");
+    }
+    return vec[size - 1];
+}
+
+template <typename T>
+size_t Collections::Vector<T>::getSize()const{
+    return size;
+}
+
+template <typename T>
+size_t Collections::Vector<T>::getCapacity()const{
+    return capacity;
+}
+
+template <typename T>
+bool Collections::Vector<T>::empty()const{
+    return size == 0;
+}
+
+template <typename T>
+void Collections::Vector<T>::shrinkToFit(){
+    if (vec == nullptr){
+        throw std::bad_alloc();
+    }
+    T* new_vec = (T*)realloc(vec, size * sizeof(T));
+    if (new_vec == nullptr){
+        throw std::bad_alloc();
+    }
+    vec = new_vec;
+    capacity = size;
+}
+
+template <typename T>
+void Collections::Vector<T>::reserveSpace(size_t extra_capacity){
+    if (vec == nullptr){
+        throw std::bad_alloc();
+    }
+    T* new_vec = (T*)realloc(vec, (capacity + extra_capacity) * sizeof(T));
+    if (new_vec == nullptr){
+        throw std::bad_alloc();
+    }
+    vec = new_vec;
+    capacity += extra_capacity;
+}
+
+template <typename T>
+void Collections::Vector<T>::push_back(const T& value){
+    if (vec == nullptr){
+        init_vector();
+    }
+    if (size >= capacity){
+        reserveSpace(capacity);
+        capacity *= 2;
+    }
+    vec[size] = value;
+    size++;
 }
--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int main(){
     Collections::Vector<int> vec4 = {1, 2, 3, 4};
     Collections::Vector<double> vec5 = {11.55, 22.3, 6.3123};
     Collections::Vector<std::string> vec6 = {"abba", "essa", "adasda"};
-
-    
+    vec3.push_back("aaa");
+    for (int i = 0 ; i < vec3.getSize(); i++){
+        std::cout << vec3[i]<<std::endl;
+    }
     return 0;
 }
