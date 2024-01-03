47fb01cf617be3437dfb0cce9bdd47450adf13fd
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2024-01-04 00:00:18 +0100

    switch implementation to Binary Search Tree, added few declarations

--- Set.h
+++ Set.h
@@ -12,41 +12,175 @@ namespace Collections{
     class Set{
         public:
             //Constructors and destructors:
-            Set(): size(), capacity(), set(nullptr) {} //default constructor
-            Set(std::initializer_list<T> values): //constructor with values: Collections::set<int> vec = {1,2,3,4,5}
-            size(values.size()), 
-            capacity(values.size() * 2), 
-            set((T*)malloc(capacity * sizeof(T))){ //dodać usuwanie powtarzających sie
-                size_t i = 0;
+            Set(): size(), root(nullptr) {} //default constructor
+            Set(std::initializer_list<T> values): size(), root(nullptr){ //constructor with values: Collections::set<int> set = {1,2,3,4,5}
                 for (const T& value : values){
-                    set[i++] = value;
+                    root = insert(root, value);
                 }
             }
             ~Set(){ //default destructor
-                free(set);
+                //space to implemet destructor using clear function
             }
             //Access to non-const sets:
-            T& operator[](size_t index); //access element using [index]
-            T& at(size_t index); //access element by index
-            T& getFront(); //get first element
-            T& getBack(); //get last element
+            T* find(const T& value); //returns the iterator to the element specified if found, else return nullptr
+            int count(const T& value); //returns 1 if the specified element is found, else 0
             //Capacity getters:
-           / size_t getSize()const; //returns size
-           / size_t getCapacity()const; //returns capacity
-           / bool empty()const; //returns true if empty
+            size_t get_size()const; //returns size
+            bool empty()const; //returns true if empty
             //Modifiers:
-           / void shrinkToFit(); //shrinks capacity to size
-           / void reserveSpace(size_t extra_space); //reserve extra memory
-           / void clear(); //clear all values
+            void insert(const T& value); //insert the specified element in the set
+            void erase(const T& element_to_delete); //deletion of an element with a set value, if not found return nullptr
+            void clear(); //deletes/clears all the elements from the set.
+            void swap(const T* set_to_swap); //swaps the elements of the two sets
+            void display();
             //Iterators:
-           / T* begin(); //returns pointer to the first element
-           / T* end(); //returns pointer to the last element
+            T* begin(); //returns pointer to the first element
+            T* end(); //returns pointer to the last element
         private:
-            void init_set();
-            void sort();
-            bool isUnique();
-            size_t capacity;
+            struct TreeNode{
+                T data;
+                TreeNode* left;
+                TreeNode* right;
+                TreeNode(const T& value) : data(value), left(nullptr), right(nullptr) {}
+            };
             size_t size;
-            T* vec;
+            TreeNode* root;
+
+            TreeNode* insert(TreeNode* node, const T& value){
+                if (node == nullptr){
+                    size++;
+                    return new TreeNode(value);
+                }
+                if (value < node->data){
+                    node->left = insert(node->left, value);
+                }else if (value > node->data){
+                    node->right = insert(node->right, value);
+                }
+                //if value already exists, skip
+                return node;
+            }
+
+            TreeNode* find(TreeNode* node, const T& value)const{
+                if (node == nullptr || node->data == value){
+                    return node;
+                }else if( value < node->data){
+                    return find(node->left, value);
+                }else{ //value > node->data
+                    return find(node->right, value);
+                }
+            }
+
+            void display(TreeNode* node){
+                if (node != nullptr)
+                {
+                    display(node->left);
+                    std::cout<<node->data<<" ";
+                    display(node->right);
+                }
+            }
+
+            TreeNode* erase(TreeNode* root2, const T& value){
+                if (root2 == nullptr){
+                    return root2;
+                }
+
+                // Recursive calls for ancestors of
+                // node to be deleted
+                if (value < root2->data){
+                    root2->left = erase(root2->left, value);
+                    return root2;
+                }else if (value > root2->data){
+                    root2->right = erase(root2->right, value);
+                    return root2;
+                }
+
+                // We reach here when root is the node
+                // to be deleted.
+
+                // If one of the children is empty
+                if (root2->left == nullptr){
+                    TreeNode* temp = root2->right;
+                    delete root2;
+                    size--;
+                    return temp;
+                }else if (root2->right == nullptr){
+                    TreeNode* temp = root2->left;
+                    delete root2;
+                    size--;
+                    return temp;
+                }
+
+                // If both children exist
+                else{
+                    TreeNode* successorParent = root2;
+
+                    //Find successor
+                    TreeNode* successor = root2->right;
+                    while (successor->left != nullptr){
+                        successorParent = successor;
+                        successor = successor->left;
+                    }
+
+                    // Delete successor.  Since successor
+                    // is always left child of its parent
+                    // we can safely make successor's right
+                    // right child as left of its parent.
+                    // If there is no succ, then assign
+                    // succ->right to succParent->right
+                    if (successorParent != root2){
+                        successorParent->left = successor->right;
+                    }else{
+                        successorParent->right = successor->right;
+                    }
+
+                    // Copy Successor Data to root
+                    root2->data = successor->data;
+
+                    delete successor;
+                    size--;
+                    return root2;
+                }
+            }
     };
+}
+
+template <typename T>
+T* Collections::Set<T>::find(const T& value){
+    TreeNode* node = find(root, value);
+    return &node->data;
+}
+
+template <typename T>
+int Collections::Set<T>::count(const T& value){
+    TreeNode* node = find(root, value);
+    return (node != nullptr)? 1 : 0;
+}
+
+template <typename T>
+size_t Collections::Set<T>::get_size()const{
+    return size;
+}
+
+template <typename T>
+bool Collections::Set<T>::empty()const{
+    return size == 0;
+}
+
+template <typename T>
+void Collections::Set<T>::insert(const T& value){
+    root = insert(root, value);
+}
+
+template <typename T>
+void Collections::Set<T>::erase(const T& value){
+    root = erase(root, value);
+}
+
+template <typename T>
+void Collections::Set<T>::display()
+{
+    if (root == nullptr){
+        throw std::runtime_error("Can not display empty set");
+    }
+    display(root);
 }
--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
-#include <stdlib.h>
 #include <iostream>
 #include "Vector.h"
+#include "Set.h"  // Replace with the actual filename of your header
 
 
-int main(){
-    Collections::Vector<int> vec1;
-    Collections::Vector<double> vec2;
-    Collections::Vector<std::string> vec3;
-    Collections::Vector<int> vec4 = {1, 2, 3, 4};
-    Collections::Vector<double> vec5 = {11.55, 22.3, 6.3123};
-    Collections::Vector<std::string> vec6 = {"abba", "essa", "adasda"};
-    vec6.push_front("aaa");
-    vec6.popAtIndex(0);
-    vec6.clear();
-        vec6.push_front("aaa");
-    for (int i = 0 ; i < vec6.getSize(); i++){
-        std::cout << vec6[i]<<" ";
+int main() {
+    Collections::Set <int> set = {2,3,4,5,6,7,99};
+    set.insert(1);
+    set.insert(2);
+    set.insert(1);
+    set.insert(4);
+    set.erase(7);
+    set.erase(7);
+    set.display();
+    auto it = set.find(0);
+    if (it != nullptr){
+        std::cout<<*it;
     }
-    std::cout<<"AA";
+    int isi = set.count(5);
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<set.get_size();
+    std::cout<<set.empty();
     return 0;
 }
