5a51b89ee1bb5216e8276289502ceb6043fd3240
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2024-03-12 22:05:24 +0100

    changed private variables names to: var_

--- Queue.h
+++ Queue.h
@@ -12,8 +12,8 @@ namespace Collections{
     class Queue{
         public:
             //Constructors and destructors:
-            Queue(): size(), front(nullptr), rear(nullptr) {} //default constructor
-            Queue(std::initializer_list<T> values): size(), front(nullptr), rear(nullptr){ //constructor with values: Collections::set<int> set = {1,2,3,4,5}
+            Queue(): size_(), front_(nullptr), rear_(nullptr) {} //default constructor
+            Queue(std::initializer_list<T> values): size_(), front_(nullptr), rear_(nullptr){ //constructor with values: Collections::set<int> set = {1,2,3,4,5}
                 for (const T& value : values){
                     enqueue(value);
                 }
@@ -44,9 +44,9 @@ namespace Collections{
                 Node* next;
                 Node(const T& value) : value(value), next(nullptr) {}
             };
-            size_t size;
-            Node* front;
-            Node* rear;
+            size_t size_;
+            Node* front_;
+            Node* rear_; 
 
             Node* newNode(T value){
                 try{
@@ -62,99 +62,99 @@ namespace Collections{
 
 template <typename T>
 T& Collections::Queue<T>::get_front(){
-    if (front == nullptr){ //queue was empty
+    if (front_ == nullptr){ //queue was empty
         throw std::runtime_error("Queue is empty, can not get first element");
     }
-    return front->value;
+    return front_->value;
 }
 
 template <typename T>
 T& Collections::Queue<T>::get_back(){
-    if (rear == nullptr){ //queue was empty
+    if (rear_ == nullptr){ //queue was empty
         throw std::runtime_error("Queue is empty, can not get last element");
     }
-    return rear->value;
+    return rear_->value;
 }
 
 template <typename T>
 size_t Collections::Queue<T>::get_size()const{
-    return size;
+    return size_;
 }
 
 template <typename T>
 bool Collections::Queue<T>::empty()const{
-    return (size == 0)? true : false;
+    return (size_ == 0)? true : false;
 }
 
 template <typename T>
 void Collections::Queue<T>::enqueue(const T& value){
     Node* node = newNode(value);
-    if (front == nullptr){ //queue was empty
+    if (front_ == nullptr){ //queue was empty
         // initialize both front and rear
-        front = node; 
-        rear = node;
+        front_ = node; 
+        rear_ = node;
     }else{
         // update rear
-        rear->next = node; 
-        rear = node;
+        rear_->next = node; 
+        rear_ = node;
     }
-    size++;
+    size_++;
 }
 
 template <typename T>
 void Collections::Queue<T>::dequeue(){
-    if (front == nullptr){ //queue was empty
+    if (front_ == nullptr){ //queue was empty
         throw std::runtime_error("Can not remove from empty Queue");
     }
-    Node* temp = front;
-    front = front->next;
-    if (front == nullptr){
-        rear = nullptr;
+    Node* temp = front_;
+    front_ = front_->next;
+    if (front_ == nullptr){
+        rear_ = nullptr;
     }
     delete temp;
-    size --;
+    size_ --;
 }
 
 template <typename T>
 void Collections::Queue<T>::clear(){
-    while (front != nullptr){
+    while (front_ != nullptr){
         dequeue();
     }
 }
 
 template <typename T>
 void Collections::Queue<T>::swap(Collections::Queue<T>& que_to_swap){
-    Node* temp_front = this->front;
-    this->front = que_to_swap.front;
-    que_to_swap.front = temp_front;
+    Node* temp_front = this->front_;
+    this->front_ = que_to_swap.front_;
+    que_to_swap.front_ = temp_front;
 
-    Node* temp_rear = this->rear;
-    this->rear = que_to_swap.rear;
-    que_to_swap.rear = temp_rear;
+    Node* temp_rear = this->rear_;
+    this->rear_ = que_to_swap.rear_;
+    que_to_swap.rear_ = temp_rear;
 
-    size_t temp_size = this->size;
-    this->size = que_to_swap.size;
-    que_to_swap.size = temp_size;
+    size_t temp_size = this->size_;
+    this->size_ = que_to_swap.size_;
+    que_to_swap.size_ = temp_size;
 }
 
 template <typename T>
 void Collections::Queue<T>::swap(Collections::Queue<T>& que1_to_swap, Collections::Queue<T>& que2_to_swap){
-    Node* temp_front = que1_to_swap.front;
-    que1_to_swap.front = que2_to_swap.front;
-    que2_to_swap.front = temp_front;
+    Node* temp_front = que1_to_swap.front_;
+    que1_to_swap.front_ = que2_to_swap.front_;
+    que2_to_swap.front_ = temp_front;
 
-    Node* temp_rear = que1_to_swap.rear;
-    que1_to_swap.rear = que2_to_swap.rear;
-    que2_to_swap.rear = temp_rear;
+    Node* temp_rear = que1_to_swap.rear_;
+    que1_to_swap.rear_ = que2_to_swap.rear_;
+    que2_to_swap.rear_ = temp_rear;
 
-    size_t temp_size = que1_to_swap.size;
-    que1_to_swap.size = que2_to_swap.size;
-    que2_to_swap.size = temp_size;
+    size_t temp_size = que1_to_swap.size_;
+    que1_to_swap.size_ = que2_to_swap.size_;
+    que2_to_swap.size_ = temp_size;
 }
 
 template <typename T>
 void Collections::Queue<T>::display()const{
-    Node* current = this->front;
+    Node* current = this->front_;
     while (current != nullptr){
         std::cout << current->value << " ";
         current = current->next;
@@ -163,7 +163,7 @@ void Collections::Queue<T>::display()const{
 
 template <typename T>
 void Collections::Queue<T>::display(const Collections::Queue<T>& que_to_display){
-    Node* current = que_to_display.front;
+    Node* current = que_to_display.front_;
     while (current != nullptr){
         std::cout << current->value << " ";
         current = current->next;
@@ -172,16 +172,16 @@ void Collections::Queue<T>::display(const Collections::Queue<T>& que_to_display)
 
 template <typename T>
 T* Collections::Queue<T>::begin(){
-    if (this->front == nullptr){
+    if (this->front_ == nullptr){
         return nullptr;
     }
-    return &front->value;
+    return &front_->value;
 }
 
 template <typename T>
 T* Collections::Queue<T>::end(){
-    if (this->rear == nullptr){
+    if (this->rear_ == nullptr){
         return nullptr;
     }
-    return &rear->value;
+    return &rear_->value;
 }
